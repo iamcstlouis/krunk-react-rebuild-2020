bf7faf1240d8c1079a539bd8135c068d07fe61bd
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 00:35:50 +0100

    Updates Navigation styles and adds Bootstrap CSS CDN to project

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -14,76 +14,78 @@ const Navigation = () => {
     return (
         <nav className='top-nav'>
             <Container>
-                <div className='logo-wrapper' onClick={() => { open ? setOpen(!open) : setOpen(open) }}>
-                    <Link to='/' exact className='top-nav-link'>
-                        <img
-                            src={logo}
-                            alt='Krunks Burgers Logo'
-                            className='top-nav-logo'
-                        />
-                    </Link>
-                </div>
-
-                {/* <!-- ***** Hamburger Toggler ***** --> */}
-                <div className='hamburger-btn' onClick={() => { setOpen(!open) }}>
-                    <div className={`hamburger-toggler ${open ? 'open' : ''}`}>
-                        <div className={`bar bar__top ${open ? 'open' : ''}`}></div>
-                        <div className={`bar bar__mid ${open ? 'open' : ''}`}></div>
-                        <div className={`bar bar__btm ${open ? 'open' : ''}`}></div>
-                    </div>
-                </div>
-
-                {/* <!-- ***** Mobile Menu ***** --> */}
-
-                <div id='mobile-menu' className={`mobile-menu ${open ? 'open' : ''}`} aria-hidden='true'>
-                    <div className='mobile-menu-links'>
-                        <NavLink exact to='/' onClick={() => { setOpen(!open) }}>Home</NavLink>
-                        <NavLink to='/krunk-burgers' onClick={() => { setOpen(!open) }}>Menu</NavLink>
-                        <NavLink to='/about' onClick={() => { setOpen(!open) }}>About</NavLink>
-                        <NavLink to='/contact' onClick={() => { setOpen(!open) }}>Contact</NavLink>
+                <div className="content-wrapper">
+                    <div className='logo-wrapper' onClick={() => { open ? setOpen(!open) : setOpen(open) }}>
+                        <Link to='/' exact className='top-nav-link'>
+                            <img
+                                src={logo}
+                                alt='Krunks Burgers Logo'
+                                className='top-nav-logo'
+                            />
+                        </Link>
                     </div>
 
-                    <div className='socials'>
-                        <a href='https://www.facebook.com/Krunk-Burgers-442165483212032/' rel="noopener noreferrer" target='_blank' onClick={() => { setOpen(!open) }}>
-                            <i className='fab fa-facebook'></i>
-                        </a>
-
-                        <a href='https://www.instagram.com/krunkburgers/?hl=en' rel="noopener noreferrer" target='_blank' onClick={() => { setOpen(!open) }}>
-                            <i className='fab fa-instagram'></i>
-                        </a>
+                    {/* <!-- ***** Hamburger Toggler ***** --> */}
+                    <div className='hamburger-btn' onClick={() => { setOpen(!open) }}>
+                        <div className={`hamburger-toggler ${open ? 'open' : ''}`}>
+                            <div className={`bar bar__top ${open ? 'open' : ''}`}></div>
+                            <div className={`bar bar__mid ${open ? 'open' : ''}`}></div>
+                            <div className={`bar bar__btm ${open ? 'open' : ''}`}></div>
+                        </div>
                     </div>
 
-                    {/* <DeliverooWidget /> */}
-                </div>
+                    {/* <!-- ***** Mobile Menu ***** --> */}
 
-                {/* <!-- Desktop Links-- > */}
-                <div className='desktop-links'>
-                    <ul className='menu-links'>
-                        <li>
-                            <router-link to='/' exact className='topNavBtn'>Home</router-link>
-                        </li>
-                        <li>
-                            <router-link to='/krunk-burgers' exact className='topNavBtn'>Menu</router-link>
-                        </li>
-                        <li>
-                            <router-link to='/about' exact className='topNavBtn'>About</router-link>
-                        </li>
-                        <li>
-                            <router-link to='/contact' exact className='topNavBtn'>Contact</router-link>
-                        </li>
-                    </ul>
+                    <div id='mobile-menu' className={`mobile-menu ${open ? 'open' : ''}`} aria-hidden='true'>
+                        <div className='mobile-menu-links'>
+                            <NavLink exact to='/' onClick={() => { setOpen(!open) }}>Home</NavLink>
+                            <NavLink to='/krunk-burgers' onClick={() => { setOpen(!open) }}>Menu</NavLink>
+                            <NavLink to='/about' onClick={() => { setOpen(!open) }}>About</NavLink>
+                            <NavLink to='/contact' onClick={() => { setOpen(!open) }}>Contact</NavLink>
+                        </div>
 
-                    <div className='socials'>
-                        <a href='https://www.facebook.com/Krunk-Burgers-442165483212032/' rel="noopener noreferrer" target='_blank'>
-                            <i className='fab fa-facebook'></i>
-                        </a>
+                        <div className='socials'>
+                            <a href='https://www.facebook.com/Krunk-Burgers-442165483212032/' rel="noopener noreferrer" target='_blank' onClick={() => { setOpen(!open) }}>
+                                <i className='fab fa-facebook'></i>
+                            </a>
 
-                        <a href='https://www.instagram.com/krunkburgers/?hl=en' rel="noopener noreferrer" target='_blank'>
-                            <i className='fab fa-instagram'></i>
-                        </a>
+                            <a href='https://www.instagram.com/krunkburgers/?hl=en' rel="noopener noreferrer" target='_blank' onClick={() => { setOpen(!open) }}>
+                                <i className='fab fa-instagram'></i>
+                            </a>
+                        </div>
+
+                        {/* <DeliverooWidget /> */}
+                    </div>
+
+                    {/* <!-- Desktop Links-- > */}
+                    <div className='desktop-links'>
+                        <ul className='menu-links'>
+                            <li>
+                                <NavLink to='/' exact className='topNavBtn'>Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/krunk-burgers' className='topNavBtn'>Menu</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/about' className='topNavBtn'>About</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to='/contact' className='topNavBtn'>Contact</NavLink>
+                            </li>
+                        </ul>
+
+                        <div className='socials'>
+                            <a href='https://www.facebook.com/Krunk-Burgers-442165483212032/' rel="noopener noreferrer" target='_blank'>
+                                <i className='fab fa-facebook'></i>
+                            </a>
+
+                            <a href='https://www.instagram.com/krunkburgers/?hl=en' rel="noopener noreferrer" target='_blank'>
+                                <i className='fab fa-instagram'></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
-            </Container >
+            </Container>
         </nav>
     )
 }
