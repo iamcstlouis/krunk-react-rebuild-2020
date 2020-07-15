903c9f63d8f0d896fe8fdd9487591ca41ff5b099
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 15:17:58 +0100

    Adjusts placement of ComicQuote and Deliveroo widget outside of Home route

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Header from './components/Header'
 import { Route, Switch, useLocation } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 import Home from './pages/Home'
 import ComicQuote from './components/ComicQuote';
+import DeliverooWidget from './components/DeliverooWidget';
 import Footer from './components/Footer';
 
 import './assets/scss/base.scss';
@@ -15,19 +17,27 @@ function App() {
   const location = useLocation();
   const pathname = location.pathname;
   const menuPage = pathname === '/menu'
+  const HomePage = pathname === '/'
 
   return (
     <React.Fragment>
       <Header />
 
-      <main className='main'>
+      <Switch>
+        <Route exact path='/' component={Home} />
 
-        {menuPage ? <ComicQuote /> : null}
-        <Switch>
-          <Route exact path='/' component={Home} />
+        <main className='main'>
+          <div className='top-section-wrapper'>
+            <Container>
+              <div className="content-wrapper">
+                {menuPage ? <ComicQuote /> : null}
+                {!HomePage ? <DeliverooWidget /> : null}
+              </div>
+            </Container>
+          </div>
           <Route path='menu' component={Home} />
-        </Switch>
-      </main>
+        </main>
+      </Switch>
 
       <Footer />
     </React.Fragment>
