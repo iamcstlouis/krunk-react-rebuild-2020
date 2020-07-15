b6d697bafdd409382eb7da8ddd2b2f0fb41ee7e2
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 15:45:02 +0100

    Adds SubMenu component to Menu route

--- src/pages/Menu/MainContent/SubMenu/index.js
+++ src/pages/Menu/MainContent/SubMenu/index.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import subMenuBangImage from '../../../../images/menu/icn_sub_menu_bang.svg'
+
+import './styles.scss'
 
 const SubMenu = () => {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className='sub-menu'>
+            <h1>Menu</h1>
+            <div className='links'>
+                <NavLink to='/krunk-burgers' exact data-aos='fade-down' data-aos-delay='100'>Burgers</NavLink>
+                <span>
+                    <img
+                        src={subMenuBangImage}
+                        alt='Sub Menu Separator'
+                        className='separator'
+                        data-aos='fade-down'
+                        data-aos-delay='200'
+                    />
+                </span>
+                <NavLink to='/krunk-wings' exact data-aos='fade-down' data-aos-delay='300'>Wings</NavLink>
+                <span>
+                    <img
+                        src={subMenuBangImage}
+                        alt='Sub Menu Separator'
+                        className='separator'
+                        data-aos='fade-down'
+                        data-aos-delay='400'
+                    />
+                </span>
+                <NavLink to='/krunk-shakes' exact data-aos='fade-down' data-aos-delay='500'>Shakes</NavLink>
+                <span>
+                    <img
+                        src={subMenuBangImage}
+                        alt='Sub Menu Separator'
+                        className='separator'
+                        data-aos='fade-down'
+                        data-aos-delay='600'
+                    />
+                </span>
+                <NavLink to='/krunk-desserts' exact data-aos='fade-down' data-aos-delay='700'>Desserts</NavLink>
+            </div>
         </div>
     )
 }
