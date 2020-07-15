b651315dc155c0e77559bfae3b697155b4f65315
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 15:08:01 +0100

    Adds Comic Quote to menu page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 
 import Header from './components/Header'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 
 import Home from './pages/Home'
+import ComicQuote from './components/ComicQuote';
 import Footer from './components/Footer';
 
-import './assets/scss/reset.scss';
 import './assets/scss/base.scss';
-
+import './assets/scss/reset.scss';
 
 function App() {
+
+  const location = useLocation();
+  const pathname = location.pathname;
+  const menuPage = pathname === '/menu'
+
   return (
     <React.Fragment>
       <Header />
 
-      <Switch>
-        <Route exact path='/' component={Home} />
-      </Switch>
+      <main className='main'>
+
+        {menuPage ? <ComicQuote /> : null}
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='menu' component={Home} />
+        </Switch>
+      </main>
 
       <Footer />
     </React.Fragment>
---
+++ src/components/ComicQuote/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './styles.scss'
+
+const ComicQuote = () => {
+    return (
+        <div class="comic-text">
+            <div class="content-wrapper">
+                <h3>Meanwhile in the kitchen...</h3>
+            </div>
+        </div>
+    )
+}
+
+export default ComicQuote
