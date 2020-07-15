32032baae03f3abf6e51c29a191dfdbd76001c69
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 22:57:03 +0100

    Adds WingMenu data to the list

--- src/pages/Menu/MainContent/MenuContainer/WingsMenu/index.js
+++ src/pages/Menu/MainContent/MenuContainer/WingsMenu/index.js
@@ -1,48 +1,59 @@
 import React from 'react'
 
 import wingsHeroImage from '../../../../../images/menu/wings/wings.jpg'
+import MenuData from '../../MenuData'
 
 import './styles.scss'
 
 const WingsMenu = () => {
     return (
-        <div className='main-content'>
-            <div className='img-hdr-wrapper'>
-                <div className='img-wrapper'>
+        <div className='wings-menu'>
+            <div className='wings-menu__img-hdr-wrapper'>
+                <div className='wings-menu__img-wrapper'>
                     <img
                         src={wingsHeroImage}
                         alt='Krunks Wings'
-                        className='featured-img'
+                        className='wings-menu__featured-img'
                         data-aos='fade-up'
                         data-aos-delay='1300'
                     />
                 </div>
 
-                <div className='wings-hdr-wrapper'>
-                    <div className='wings-flavours-hdr'>
-                        <div className='hdr-wrapper'>
-                            <h2 data-aos='fade-up' data-aos-delay='1450'>Flavours</h2>
+                <div className='wings-menu__outer-hdr-wrapper'>
+                    <div className='wings-menu__inner-hdr-wrapper'>
+                        <div className='wings-menu__hdr-wrapper'>
+                            <h2 className='wings-menu__hdr' data-aos='fade-up' data-aos-delay='1450'>Flavours</h2>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div className='wing-flavours-list'>
-                <ul id='flavours'>
-                    <li
-              data-aos='fade-up'
-              data-aos-delay='1450'
-            >
-              <a
-                        href='https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP'
-                        target='_blank'
-                        data-aos='fade-up'
-                        data-aos-delay='450'
-                    ></a>
-            </li>
-          </ul>
+            <div className='wings-menu__flavours-list'>
+                <ul className='wings-menu__flavours'>
+                    {
+                        MenuData.wings.map((wing) => {
+                            return (
+                                <li
+                                    data-aos='fade-up'
+                                    data-aos-delay='1450'
+                                    key={wing.id}
+                                >
+                                    <a
+                                        href='https://deliveroo.co.uk/menu/london/croydon/krunk?day=today&postcode=SE256NX&time=ASAP'
+                                        rel='noopener noreferrer'
+                                        target='_blank'
+                                        data-aos='fade-up'
+                                        data-aos-delay='450'
+                                    >
+                                        {wing.flavour}
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
         </div>
-      </div >
     )
 }
 
