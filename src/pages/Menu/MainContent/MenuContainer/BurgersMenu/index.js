6d5d049fffd1d33e8280dbb4442e12d64d868f89
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 18:46:04 +0100

    Adds mobile menu tab

--- src/pages/Menu/MainContent/MenuContainer/BurgersMenu/index.js
+++ src/pages/Menu/MainContent/MenuContainer/BurgersMenu/index.js
@@ -9,6 +9,8 @@ import MealsSeparator from '../../MealsSeparator'
 const BurgersMenu = () => {
     return (
         <div className='cards'>
+
+            <h2 class="cards__mobile-menu-tab" data-aos="fade-left" data-aos-delay="1300">Beef</h2>
             <div className="cards cards__beef">
                 {
                     MenuData.burgers.beef.map((burger) => {
@@ -21,6 +23,7 @@ const BurgersMenu = () => {
 
             <MealsSeparator />
 
+            <h2 class="cards__mobile-menu-tab" data-aos="fade-left" data-aos-delay="1300">Chicken</h2>
             <div className="cards cards__chicken">
                 {
                     MenuData.burgers.chicken.map((burger) => {
@@ -33,7 +36,7 @@ const BurgersMenu = () => {
 
             <MealsSeparator />
 
-
+            <h2 class="cards__mobile-menu-tab" data-aos="fade-left" data-aos-delay="1300">Vegan</h2>
             <div className="cards cards__vegan">
                 {
                     MenuData.burgers.vegan.map((burger) => {
