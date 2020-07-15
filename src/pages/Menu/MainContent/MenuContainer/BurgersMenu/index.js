c95b48ac6d1a5b43e7278e238ceebdeb88eef2ca
Chantal St Louis <misscstlouis@gmail.com>
2020-07-15 18:22:54 +0100

    Adds Chicken and vegan burgers to the menu

--- src/pages/Menu/MainContent/MenuContainer/BurgersMenu/index.js
+++ src/pages/Menu/MainContent/MenuContainer/BurgersMenu/index.js
@@ -8,13 +8,35 @@ import './styles.scss'
 const BurgersMenu = () => {
     return (
         <div className='cards'>
-            {
-                MenuData.burgers.beef.map((burger) => {
-                    return (
-                        <BurgerCard burgerImage={burger.burgerImg} burgerImageAlt={burger.alt} burgerName={burger.burgerName} burgerDesc={burger.burgerDesc} key={burger.id} />
-                    )
-                })
-            }
+            <div className="cards cards__beef">
+                {
+                    MenuData.burgers.beef.map((burger) => {
+                        return (
+                            <BurgerCard burgerImage={burger.burgerImg} burgerImageAlt={burger.alt} burgerName={burger.burgerName} burgerDesc={burger.burgerDesc} key={burger.id} />
+                        )
+                    })
+                }
+            </div>
+
+            <div className="cards cards__chicken">
+                {
+                    MenuData.burgers.chicken.map((burger) => {
+                        return (
+                            <BurgerCard burgerImage={burger.burgerImg} burgerImageAlt={burger.alt} burgerName={burger.burgerName} burgerDesc={burger.burgerDesc} key={burger.id} />
+                        )
+                    })
+                }
+            </div>
+
+            <div className="cards cards__vegan">
+                {
+                    MenuData.burgers.vegan.map((burger) => {
+                        return (
+                            <BurgerCard burgerImage={burger.burgerImg} burgerImageAlt={burger.alt} burgerName={burger.burgerName} burgerDesc={burger.burgerDesc} key={burger.id} />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
